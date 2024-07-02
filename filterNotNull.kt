7d50afca83fe7cb9516418889378b3d3d966cd5e
KHUSI PANDEY <128993461+khusipandey@users.noreply.github.com>
2024-07-02 21:14:20 +0530

    Update filterNotNull.kt

--- filterNotNull.kt
+++ filterNotNull.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val list= listOf(1, 2, null, 4, null, 6)
+    val list= listOf(2,5,6,null,4,null,36,null,89)
     val notnullList= filterNonNulllist(list)
     println("Original list: $list")
     println("Filtered list: $notnullList")
